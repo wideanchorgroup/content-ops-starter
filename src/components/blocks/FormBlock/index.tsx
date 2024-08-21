78a684eb695c7731b6147d4eb9b58d3707861554
wideanchorgroup <info@wideanchor.com>
2024-08-20 23:41:25 -0400

    Update index.tsx

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -35,7 +35,7 @@ export default function FormBlock(props) {
         // alert(`Form data: ${JSON.stringify(value)}`);
 
         const form = event.target
-        fetch('/thankyou', {
+        fetch('/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: encode({
@@ -46,66 +46,66 @@ export default function FormBlock(props) {
             .then(() => console.log("Success!"))
             .catch(error => console.log(error));
 
+        setSubmitted(true);
     }
 
-        return (
-            <form
-                className={classNames(
-                    'sb-component',
-                    'sb-component-block',
-                    'sb-component-form-block',
-                    className,
-                    styles?.self?.margin ? mapStyles({ margin: styles?.self?.margin }) : undefined,
-                    styles?.self?.padding ? mapStyles({ padding: styles?.self?.padding }) : undefined,
-                    styles?.self?.borderWidth && styles?.self?.borderWidth !== 0 && styles?.self?.borderStyle !== 'none'
-                        ? mapStyles({
-                            borderWidth: styles?.self?.borderWidth,
-                            borderStyle: styles?.self?.borderStyle,
-                            borderColor: styles?.self?.borderColor ?? 'border-primary'
-                        })
-                        : undefined,
-                    styles?.self?.borderRadius ? mapStyles({ borderRadius: styles?.self?.borderRadius }) : undefined
-                )}
-                name={elementId}
-                id={elementId}
-                method="POST"
-                action="https://americanmedrobotics.com"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={handleSubmit}
-                ref={formRef}
-                data-sb-field-path={fieldPath}
+    return (
+        <form
+            className={classNames(
+                'sb-component',
+                'sb-component-block',
+                'sb-component-form-block',
+                className,
+                styles?.self?.margin ? mapStyles({ margin: styles?.self?.margin }) : undefined,
+                styles?.self?.padding ? mapStyles({ padding: styles?.self?.padding }) : undefined,
+                styles?.self?.borderWidth && styles?.self?.borderWidth !== 0 && styles?.self?.borderStyle !== 'none'
+                    ? mapStyles({
+                        borderWidth: styles?.self?.borderWidth,
+                        borderStyle: styles?.self?.borderStyle,
+                        borderColor: styles?.self?.borderColor ?? 'border-primary'
+                    })
+                    : undefined,
+                styles?.self?.borderRadius ? mapStyles({ borderRadius: styles?.self?.borderRadius }) : undefined
+            )}
+            name={elementId}
+            id={elementId}
+            method="POST"
+            action="https://americanmedrobotics.com"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={handleSubmit}
+            ref={formRef}
+            data-sb-field-path={fieldPath}
+        >
+
+            <input type="hidden" name="form-name" value={elementId} />
+            <p hidden>
+                <label>
+                    Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+                </label>
+            </p>
+            <div
+                className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
+                {...(fieldPath && { 'data-sb-field-path': '.fields' })}
             >
-
                 <input type="hidden" name="form-name" value={elementId} />
-                <p hidden>
-                    <label>
-                        Don’t fill this out: <input name="bot-field" onChange={handleChange} />
-                    </label>
-                </p>
-                <div
-                    className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
-                    {...(fieldPath && { 'data-sb-field-path': '.fields' })}
-                >
-                    <input type="hidden" name="form-name" value={elementId} />
-                    {fields.map((field, index) => {
-                        const modelName = field.__metadata.modelName;
-                        if (!modelName) {
-                            throw new Error(`form field does not have the 'modelName' property`);
-                        }
-                        const FormControl = getComponent(modelName);
-                        if (!FormControl) {
-                            throw new Error(`no component matching the form field model name: ${modelName}`);
-                        }
-                        return <FormControl key={index} {...field} {...(fieldPath && { 'data-sb-field-path': `.${index}` })} />;
-                    })}
+                {fields.map((field, index) => {
+                    const modelName = field.__metadata.modelName;
+                    if (!modelName) {
+                        throw new Error(`form field does not have the 'modelName' property`);
+                    }
+                    const FormControl = getComponent(modelName);
+                    if (!FormControl) {
+                        throw new Error(`no component matching the form field model name: ${modelName}`);
+                    }
+                    return <FormControl key={index} {...field} {...(fieldPath && { 'data-sb-field-path': `.${index}` })} />;
+                })}
+            </div>
+            {submitButton && (
+                <div className={classNames('mt-8', 'flex', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}>
+                    <SubmitButtonFormControl {...submitButton} {...(fieldPath && { 'data-sb-field-path': '.submitButton' })} />
                 </div>
-                {submitButton && (
-                    <div className={classNames('mt-8', 'flex', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}>
-                        <SubmitButtonFormControl {...submitButton} {...(fieldPath && { 'data-sb-field-path': '.submitButton' })} />
-                    </div>
-                )}
-            </form>
-        );
-    }
+            )}
+        </form>
+    );
 }
